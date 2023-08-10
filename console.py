d9d7bd4c013b29592f307b73830af8e3e5e75655
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-08-10 02:16:02 +0100

    implemented the create method

--- console.py
+++ console.py
@@ -6,10 +6,9 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """This module contains the entry point to the command interpreter"""
-    def __init__(self):
-        """Initializes the class"""
-        cmd.Cmd.__init__(self)
-        self.prompt = "(hbnb) "
+    prompt = "(hbnb) "
+    MODELS = ['BaseModel', 'User', 'Place', 'State', 'City',
+              'Amenity', 'Review']
 
     def do_EOF(self, line):
         """Exits the console"""
@@ -26,6 +25,32 @@ class HBNBCommand(cmd.Cmd):
     def postloop(self):
         """gives a new line after exicting the console"""
         print()
+
+    def do_create(self, line):
+        """TO create an instance of the base model"""
+        if not line:
+            print("** class name missing **")
+        else:
+            args = line.split()
+            command = args[0]
+            if command not in self.MODELS:
+                print("** class doesn't exist **")
+            else:
+                new_instance = self.MODELS[command]()
+                new_instance.save()
+                print(new_instance.id)
+
+    def do_show(self, line):
+        """Prints the string instance based on the class name"""
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+
+    def do_all(self, line):
+        """Prints all instances based or not on the class name"""
+
+    def do_update(self, line):
+        """ Updates an instance based on the class name and id"""
 
 
 if __name__ == "__main__":
