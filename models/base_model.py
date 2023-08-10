31ed2ba4bb164b05bfac929f1df6d65c0af48108
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-08-10 01:04:50 +0100

    changed the import of models to import whole module
    This was due to an error caused by circular import
    Therefore I ammended the code to this effect

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage
-
+import models
 
 class BaseModel:
     """
@@ -33,21 +32,21 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """str method for BaseModel Class
             Return:
                 string (str): string descriptor for BaseModel Class
         """
-        return (f"[{self.__class.__name__}] ({self.id}) {self.__dict__}")
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """Updates the updated_at attribute
         with the current datetime."""
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance.\
