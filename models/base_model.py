0f7fb2ed550aeddc557042dc0575e7fac59f9159
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-08-10 15:48:35 +0100

    reverted the imort for the models.storage back to the original one

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 class BaseModel:
     """
@@ -32,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """str method for BaseModel Class
@@ -46,7 +46,7 @@ class BaseModel:
         with the current datetime."""
 
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of an instance.\
