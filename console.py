3fac16b250be1d8085535b57415126c717b5d4f0
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-08-13 00:41:14 +0100

    minor changes

--- console.py
+++ console.py
@@ -43,6 +43,8 @@ class HBNBCommand(cmd.Cmd):
         """gives a new line after exicting the console"""
         print()
 
+
+"""
     def do_create(self, line):
         """TO create an instance of the base model"""
         if not line:
@@ -57,7 +59,27 @@ class HBNBCommand(cmd.Cmd):
                 print(new_instance.id)
             else:
                 print("** class doesn't exist **")
+"""
 
+    def do_create(self, line):
+        """TO create an instance of the base model"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+        else:
+            command = args[0]
+            if not command:
+                print("** class name missing **")  # Here
+            else:
+                model = self.MODELS.get(command)
+                if model:
+                    new_instance = model()
+                    new_instance.save()
+                    print(new_instance.id)
+                else:
+                    print("** class doesn't exist **")
+
+"""
     def do_show(self, line):
         """Prints the string instance based on the class name"""
         if not line:
@@ -79,7 +101,30 @@ class HBNBCommand(cmd.Cmd):
             print(all_objects[key])
         else:
             print("** no instance found **")
+"""
 
+    def do_show(self, line):
+        """Prints the string instance based on the class name"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        command = args[0]
+        if command not in self.MODELS:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        key = (f"{command}.{instance_id}")
+        all_objects = storage.all()
+        if key in all_objects:
+            print(all_objects[key])
+        else:
+            print("** no instance found **")
+
+"""
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id"""
         if not line:
@@ -102,7 +147,32 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
         else:
             print("** no instance found **")
+"""
 
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+        command = args[0]
+        if command not in self.MODELS:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = f"{command}.{instance_id}"
+        all_objects = storage.all()
+        if key in all_objects:
+            del all_objects[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+    
+    """
     def do_all(self, line):
         """Prints all instances based or not on the class name"""
         args = line.split()
@@ -120,7 +190,31 @@ class HBNBCommand(cmd.Cmd):
             for key, instance_id in all_objects.items():
                 if instance_id.__class__.__name__ == command:
                     print(instance_id)
+"""
 
+    def do_all(self, line):
+        """Prints all instances based or not on the class name"""
+        args = line.split()
+        if not args:
+            all_objects = storage.all()
+            instance_list = []
+            for key, instance_id in all_objects.items():
+                if instance_id.__class__.__name__ == command:
+                    instance_list.append(str(instance_id))
+            print(instance_list)  # Here
+        else:
+            command = args[0]
+            if command not in self.MODELS:
+                print("** class doesn't exist **")
+                return
+            all_objects = storage.all()
+            instance_list = []
+            for key, instance_id in all_objects.items():
+                if instance_id.__class__.__name__ == command:
+                    instance_list.append(str(instance_id))
+            print(instance_list)
+
+"""
     def do_update(self, line):
         """Updates an instance based on the class name and id"""
         args = line.split()
@@ -158,6 +252,57 @@ class HBNBCommand(cmd.Cmd):
         instance = all_objects[key]
         setattr(instance, attribute_name, attribute_value)
         instance.save()
+"""
+     def do_update(self, line):
+        """Updates an instance based on the class name and id"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        command = args[0]
+        if command not in self.MODELS:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = f"{command}.{instance_id}"
+        all_objects = storage.all()
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        attribute_name = args[2]
+        if attribute_name in ["id", "created_at", "updated_at"]:
+            print("** attribute cannot be updated **")
+            return
+
+        attribute_value = args[3]
+
+        instance = all_objects[key]
+        if hasattr(instance, attribute_name):
+            attr_type = type(getattr(instance, attribute_name))
+            try:
+                casted_value = attr_type(attribute_value)
+            except ValueError:
+                print("** invalid value type **")
+                return
+            setattr(instance, attribute_name, casted_value)
+            instance.save()
+        else:
+            print("** attribute doesn't exist **")
 
 
 if __name__ == "__main__":
