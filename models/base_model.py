49dc4ea2e0a9a5edf22a98ce2dfb4bb889610a5a
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-13 06:00:25 +0100

    fixing bugs

--- models/base_model.py
+++ models/base_model.py
@@ -18,13 +18,19 @@ class BaseModel:
             - *args: list of arguments (not used in this implementation)
             - **kwargs: dictionary of key, value pairs
         """
-        if kwargs:
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None and len(kwargs.keys()) > 0:
+            fmt = '%Y-%m-%dT%H:%M:%S.%f'
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key,
-                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != "__class__":
-                    setattr(self, key, value)
+                if key == '__class__':
+                    continue
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(value, fmt)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(value, fmt)
+                else:
+                    exec(f'self.{key} = value')
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
