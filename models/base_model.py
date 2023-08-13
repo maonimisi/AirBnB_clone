f8374ec828f99e54a39122858572ccab71dee364
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-13 10:26:02 +0100

    AirBnb Console

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,66 @@
 #!/usr/bin/python3
-"""
-This module defines the BaseModel class, which serves as the
-base class for other classes.
-"""
 
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
     """
-    The BaseModel class defines common attributes and methods
-    for other classes.
+    base_model that defines all common attributes/methods for other classes
     """
-
     def __init__(self, *args, **kwargs):
+        """init method for BaseModel Class
+
+        Attributes:
+            args (list): inputted arguments as a list.
+            kwargs (dict): inputted arguments as a dict.
+            id (str) - assign with an uuid when an instance is created.
+            created_at (time): datetime - assign with the current datetime when
+                an instance is created.
+            updated_at (time): datetime - assign with the current datetime when
+                n instance is created and it will be updated every time you
+                change your object.
         """
-        Initializes a new instance of the BaseModel class.
-        args:
-            - *args: list of argument
-            - **kwargs: dictionary of key, value pairs
-        """
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+        if len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k in ['created_at', 'updated_at']:
+                    self.__dict__[k] = datetime\
+                                       .strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == 'id':
+                    self.id = v
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
+#            models.storage.new(self)
 
     def __str__(self):
-        """
-        Returns a string representation of the BaseModel instance.
+        """str method for BaseModel Class
+
+            Return:
+                string (str): string descriptor for BaseModel Class
         """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
 
     def save(self):
         """
-        Updates the 'updated_at' attribute with the current datetime.
+        Updates the public instance attribute updated_at with the
+        current datetime
         """
         self.updated_at = datetime.now()
+#        models.storage.save()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__
+        of the instance
+
+        Return:
+            dictionary (dict): Dictionary object that contains __dict__
         """
-        Converts the BaseModel instance to a dictionary representation.
-        Includes class name, created_at, and updated_at attributes.
-        """
-        instance_dict = self.__dict__.copy()
-        instance_dict['__class__'] = self.__class__.__name__
-        instance_dict['created_at'] = self.created_at.isoformat()
-        instance_dict['updated_at'] = self.updated_at.isoformat()
-        return instance_dict
+        dictionary = self.__dict__.copy()
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["__class__"] = self.__class__.__name__
+        return dictionary
