b73f0907cd50ecd69c24229c0f54392030486abd
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-13 15:50:22 +0100

    AirBnb Console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,42 +15,44 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def __init__(self):
+        """init method for FileStorage class
+        """
+        pass
+
     def all(self):
+        """returns the dictionary __objects
         """
-        Returns a dictionary containing all the stored objects
-        """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Add a new object, obj, to __objects, the dictionary of
-        all stored objects
+        """sets in __objects the obj with key <obj class name>.id
 
-        Args:
-            obj (BaseModel): The object to be added to the dictionary of
-            stored objects
+        Attributes:
+            obj (Python object): The object to set
         """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        dictionary = obj.to_dict()
+        key = '{}.{}'.format(dictionary['__class__'], str(obj.id))
+        FileStorage.__objects[key] = obj
 
     def save(self):
+        """serializes __objects to the JSON file (path: __file_path)
         """
-        Serialize the current content of __objects into a JSON file
-        """
-        my_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w', encoding='utf-8') as file:
-            json.dump(my_dict, file)
+        dictionary = dict()
+        for key, value in FileStorage.__objects.items():
+            dictionary[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(dictionary, file)
 
     def reload(self):
-        """
-        Deserialize the content of the JSON file into the __objects dictionary
+        """deserializes the JSON file to __objects ONLY if the JASON file
+        exists, otherwise, do nothing.  Raise exception if the file doesn't
+        exist, exceptions
         """
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
                 json_load = json.load(file)
-            for key, value in json_load.items():
-                class_name = value["__class__"]
-                del value["__class__"]
-                self.__objects[key] = globals()[class_name](**value)
-        except FileNotFoundError:
+            for k, v in json_load.items():
+                FileStorage.__objects[k] = BaseModel(**v)
+        except:
             pass
