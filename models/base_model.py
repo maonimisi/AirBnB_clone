059d990b46e2c5873a7ce03cb67d9a0704c19225
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-13 06:23:37 +0100

    fixing bugs

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,8 @@ class BaseModel:
 
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
 
     def save(self):
