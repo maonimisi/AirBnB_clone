758a497138b427986d25e598df1216a09fed91a9
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-13 10:42:08 +0100

    AirBnb Console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,48 +4,66 @@ FileStorage that serializes instances to a JSON file and
 deserializes JSON file to instances:
 """
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
 
-    """Class for serializtion and deserialization of base classes"""
+    """FileStorage class for serializtion and deserialization of objects
+    into and from files
+    """
     __file_path = "file.json"
     __objects = {}
 
     def __init__(self):
+        """
+        init method for FileStorage class
+        """
         pass
 
     def all(self):
         """
         Returns a dictionary containing all the stored objects
         """
-        return type(self).__objects
+        return FileStorage.__objects
 
     def new(self, obj):
-        '''
+        """
         Add a new object, obj, to __objects, which is the dictionary of
-            all stored objects
+        all stored objects
 
         Args:
         obj(object): The object to be added to the dictionary of stored objects
-        '''
-        type(self).__objects[f'{type(obj).__name__}.{obj.id}'] = obj.to_dict()
+        """
+        my_dict = obj.to_dict()
+        key = "{}.{}".format(dictionary["__class__"], str(obj.id))
+        FileStorage.__objects[key] = obj
 
     def save(self):
         '''
         Serialize the current content of __object into a json file with
         the file path __file_path
         '''
-        with open(type(self).__file_path, mode='w', encoding='UTF8') as json_file:
-            json.dump(type(self).__objects, json_file)
+        dictionary = dict()
+        for k, v in FileStorage.__objects.items():
+            dictionary[k] = v.to_dict()
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(dictionary, file)
 
     def reload(self):
         '''
         Deserialize the content of the json file into the __objects dictionary
         '''
         try:
-            with open(f'{type(self).__file_path}', mode='r', encoding='UTF8')\
-                 as json_file:
-                type(self).__objects = json.load(json_file)
-        except FileNotFoundError:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+                json_load = json.load(file)
+            for k, v in json_load.items():
+                FileStorage.__objects[k] = BaseModel(**v)
+        except:
             pass
