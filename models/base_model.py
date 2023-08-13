7b6598feb9082697db408accf77828cb60679dd9
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-13 09:31:49 +0100

    AirBnb Console

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ base class for other classes.
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -14,13 +15,27 @@ class BaseModel:
     for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
+        args:
+            - *args: list of argument
+            - **kwargs: dictionary of key, value pairs
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
