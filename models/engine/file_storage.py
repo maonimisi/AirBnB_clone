816a685a85cbfe0a550aa9fa56a5b8a245c55ee7
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-08-13 13:25:50 +0100

    AirBnb Clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,65 +5,52 @@ deserializes JSON file to instances:
 """
 import json
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 class FileStorage:
-
-    """FileStorage class for serializtion and deserialization of objects
+    """
+    FileStorage class for serialization and deserialization of objects
     into and from files
     """
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-        """
-        init method for FileStorage class
-        """
-        pass
-
     def all(self):
         """
         Returns a dictionary containing all the stored objects
         """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """
-        Add a new object, obj, to __objects, which is the dictionary of
+        Add a new object, obj, to __objects, the dictionary of
         all stored objects
 
         Args:
-        obj(object): The object to be added to the dictionary of stored objects
+            obj (BaseModel): The object to be added to the dictionary
+            of stored objects
         """
-        dictionary = obj.to_dict()
-        key = "{}.{}".format(dictionary["__class__"], str(obj.id))
-        FileStorage.__objects[key] = obj
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
-        '''
-        Serialize the current content of __object into a json file with
-        the file path __file_path
-        '''
-        dictionary = dict()
-        for k, v in FileStorage.__objects.items():
-            dictionary[k] = v.to_dict()
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
+        """
+        Serialize the current content of __objects into a JSON file
+        """
+        dictionary = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(dictionary, file)
 
     def reload(self):
-        '''
-        Deserialize the content of the json file into the __objects dictionary
-        '''
+        """
+        Deserialize the content of the JSON file into the __objects dictionary
+        """
         try:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
                 json_load = json.load(file)
-            for k, v in json_load.items():
-                FileStorage.__objects[k] = BaseModel(**v)
-        except:
+            for key, value in json_load.items():
+                class_name = value["__class__"]
+                del value["__class__"]
+                self.__objects[key] = globals()[class_name](**value)
+        except FileNotFoundError:
             pass
